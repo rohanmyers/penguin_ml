914ee7e0f4cebe0c86451fef2e14c51c0a1a77ee
rohanmyers <rohanpeer@gmail.com>
2026-02-01 19:07:27 -0500

    fix name error

--- penguins_streamlit.py
+++ penguins_streamlit.py
@@ -9,15 +9,17 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
 st.title('Penguin Classifier')
-st.write("This app uses 6 inputs to predict the species of penguin using"
+st.write("This app uses 6 inputs to predict the species of penguin using "
          "a model built on the Palmer Penguins dataset. Use the form below"
          " to get started!")
+penguin_df = pd.read_csv('penguins.csv')
 rf_pickle = open('random_forest_penguin.pickle', 'rb')
 map_pickle = open('output_penguin.pickle', 'rb')
 rfc = pickle.load(rf_pickle)
 unique_penguin_mapping = pickle.load(map_pickle)
 rf_pickle.close()
 map_pickle.close()
+
 
 island = st.selectbox('Penguin Island', options=[
                       'Biscoe', 'Dream', 'Torgerson'])
